487cf51ae9ae7c4e442b0a6db7447cff13d59ff2
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2016-06-24 16:29:44 -0700

    Initial configuration

--- Navigation Controller/Home.m
+++ Navigation Controller/Home.m
@@ -6,13 +6,13 @@
 //  Copyright © 2016 Smartplace. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "Home.h"
 
-@interface ViewController ()
+@interface Home ()
 
 @end
 
-@implementation ViewController
+@implementation Home
 
 - (void)viewDidLoad {
     [super viewDidLoad];
